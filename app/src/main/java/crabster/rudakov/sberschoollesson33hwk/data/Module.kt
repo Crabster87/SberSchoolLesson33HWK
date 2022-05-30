2e0eb4f038a6103d6bc4017daf526c8bfe026205
Crabster87 <iar876@yandex.ru>
2022-05-30 21:03:35 +0300

    implementation Repository pattern

---
+++ app/src/main/java/crabster/rudakov/sberschoollesson33hwk/data/Module.kt
@@ -0,0 +1,21 @@
+package crabster.rudakov.sberschoollesson33hwk.data
+
+import crabster.rudakov.sberschoollesson33hwk.data.repository.WeatherRepository
+import crabster.rudakov.sberschoollesson33hwk.data.repository.WeatherRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class Module {
+
+    @Provides
+    @Singleton
+    fun provideRepository(): WeatherRepository {
+        return WeatherRepositoryImpl()
+    }
+
+}
--- app/src/main/java/crabster/rudakov/sberschoollesson33hwk/data/repository/WeatherRepository.kt
+++ app/src/main/java/crabster/rudakov/sberschoollesson33hwk/data/repository/WeatherRepository.kt
@@ -1,23 +1,10 @@
 package crabster.rudakov.sberschoollesson33hwk.data.repository
 
-import crabster.rudakov.sberschoollesson33hwk.utils.Constants
-import crabster.rudakov.sberschoollesson33hwk.data.api.RetrofitClient
 import crabster.rudakov.sberschoollesson33hwk.data.model.LocalWeather
-import dagger.hilt.android.scopes.ViewModelScoped
 import io.reactivex.Single
-import javax.inject.Inject
 
-@ViewModelScoped
-class WeatherRepository @Inject constructor() {
+interface WeatherRepository {
 
-    fun getCurrentWeather(latitude: Double, longitude: Double): Single<LocalWeather> {
-        return RetrofitClient.api.getWeather(
-            latitude,
-            longitude,
-            Constants.EXCLUDED_PARAMS,
-            Constants.API_KEY,
-            Constants.UNIT_PARAMS
-        )
-    }
+    fun getCurrentWeather(latitude: Double, longitude: Double): Single<LocalWeather>
 
 }
